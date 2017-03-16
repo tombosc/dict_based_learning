7d89f0368457b4c35b8dbd2839d165e6a2bbb893
rizar <dimabv@tut.by>
2017-03-16 13:21:36 -0400

    h5py conversion

---
+++ dictlearn/h5py_conversion.py
@@ -0,0 +1,23 @@
+import h5py
+
+from fuel.datasets.hdf5 import H5PYDataset
+
+def text_to_h5py_dataset(text_path, dst_path):
+    # The simplest is to load everything to memory first.
+    # If memory becomes an issue, this code can be optimized.
+    words = []
+    with open(text_path, 'r') as src:
+        for line in src:
+            words.extend(line.strip().split())
+
+    with h5py.File(dst_path, 'w') as dst:
+        dtype = h5py.special_dtype(vlen=bytes)
+        table = dst.create_dataset('words', (len(words),), dtype=dtype)
+        for i, word in enumerate(words):
+            table[i] = word
+
+        dst.attrs['split'] = H5PYDataset.create_split_array({
+                'train' : {
+                    'words' : (0, len(words))
+                }
+            })
---
+++ tests/test_h5py_conversion.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import tempfile
+
+from fuel.datasets.hdf5 import H5PYDataset
+
+from dictlearn.h5py_conversion import text_to_h5py_dataset
+
+def test_text_to_h5py_dataset():
+    test_dir = tempfile.mkdtemp()
+    text_path = os.path.join(test_dir, 'text.txt')
+    h5_path = os.path.join(test_dir, 'words.h5')
+    with open(os.path.join(test_dir, 'text.txt'), 'w') as dst:
+        print('abc', file=dst)
+        print('été', file=dst)
+        print('abc Δίας', file=dst)
+    text_to_h5py_dataset(text_path, h5_path)
+
+    f = H5PYDataset(h5_path, ('train',))
+    it = f.get_example_stream().get_epoch_iterator()
+    assert next(it)[0] == 'abc'
+    assert next(it)[0] == 'été'
+    assert next(it)[0] == 'abc'
+    assert next(it)[0] == 'Δίας'
