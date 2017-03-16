bee100f96eaa0e6b10ef202cb1309dacb502b6a0
rizar <dimabv@tut.by>
2017-03-16 13:55:43 -0400

    h5py conversion should be faster this way

--- dictlearn/h5py_conversion.py
+++ dictlearn/h5py_conversion.py
@@ -13,8 +13,7 @@ def text_to_h5py_dataset(text_path, dst_path):
     with h5py.File(dst_path, 'w') as dst:
         dtype = h5py.special_dtype(vlen=bytes)
         table = dst.create_dataset('words', (len(words),), dtype=dtype)
-        for i, word in enumerate(words):
-            table[i] = word
+        table[:] = words
 
         dst.attrs['split'] = H5PYDataset.create_split_array({
                 'train' : {
