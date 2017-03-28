89f2b754e4e8c390270150e9705b5780aa782fbb
Tom Bosc <bosc.tom@gmail.com>
2017-03-28 17:10:48 -0400

    configs

---
+++ bin/train_synthetic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+from dictlearn.language_model_training import train_language_model
+from dictlearn.language_model_configs import lm_config_registry
+import dictlearn.lm_configs_synthetic
+import argparse
+
+parser = argparse.ArgumentParser("Generate synthetic data and outputs in files")
+parser.add_argument("results_path", type=str)
+parser.add_argument("config_name", type=str)
+
+args = parser.parse_args()
+
+c = lm_config_registry[args.config_name]
+train_language_model(c, args.results_path, True, False)
