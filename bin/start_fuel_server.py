57dc7e91df90214ddf14103742abb4a95a8a6489
rizar <dimabv@tut.by>
2017-03-30 10:36:56 -0400

    Just align everything

--- bin/start_fuel_server.py
+++ bin/start_fuel_server.py
@@ -2,17 +2,20 @@
 
 import argparse
 import cPickle
+import logging
 
 from fuel.server import start_server
 
-def main():
-  parser = argparse.ArgumentParser("Builds a dictionary")
-  parser.add_argument("stream", help="The path to the pickled stream")
-  parser.add_argument("port", type=int, help="The port to use")
-  args = parser.parse_args()
+logger = logging.getLogger()
 
-  stream = cPickle.load(open(args.stream))
-  start_server(stream, args.port)
+def main():
+    parser = argparse.ArgumentParser("Builds a dictionary")
+    parser.add_argument("stream", help="The path to the pickled stream")
+    parser.add_argument("port", type=int, help="The port to use")
+    args = parser.parse_args()
+
+    stream = cPickle.load(open(args.stream))
+    start_server(stream, args.port)
 
 
 if __name__ == "__main__":
