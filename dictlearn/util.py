4dcb585b2998fb2cd7c7680007f512e93f013cfb
rizar <dimabv@tut.by>
2017-04-04 08:54:26 -0400

    Move masked_root_mean_square to util

--- dictlearn/util.py
+++ dictlearn/util.py
@@ -9,11 +9,24 @@ def softmax(v, T):
 def vec2str(vector):
     return "".join(map(chr, vector)).strip('\00')
 
+
 def str2vec(str_, length):
     vector = numpy.array(map(ord, str_))[:length]
     pad_length = max(0, length - len(str_))
     return numpy.pad(vector, (0, pad_length), 'constant')
 
+
 def rename(var, name):
     var.name = name
     return var
+
+
+def smart_sum(x):
+    for i in range(x.ndim):
+        x = x.sum(axis=-1)
+    return x
+
+
+def masked_root_mean_square(x, mask):
+    """Masked root mean square for a 3D tensor"""
+    return (smart_sum((x * mask[:, :, None]) ** 2) / x.shape[2] / mask.sum()) ** 0.5
