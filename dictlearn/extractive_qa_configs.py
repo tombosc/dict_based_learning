cba7e56eb4bdbc4fcef5d53de021ee45224e60b2
rizar <dimabv@tut.by>
2017-04-15 10:22:41 -0400

    configs

--- dictlearn/extractive_qa_configs.py
+++ dictlearn/extractive_qa_configs.py
@@ -14,7 +14,7 @@ qa_config_registry.set_root_config({
 
     # model
     'dim' : 128,
-    'emb_dim': 300,
+    'emb_dim' : 0,
     'learning_rate' : 0.001,
     'momentum' : 0.9,
     'grad_clip_threshold' : 5.0,
@@ -28,20 +28,28 @@ qa_config_registry.set_root_config({
 })
 
 c = qa_config_registry['root']
-c['data_path'] = '/data/lisatmp4/bahdanau/data/squad'
+c['data_path'] = '/data/lisatmp4/bahdanau/data/squad/squad_from_scratch'
 c['layout'] = 'squad'
-c['num_input_words'] = 1000
 c['mon_freq_train'] = 100
 c['grad_clip_threshold'] = 50.
 qa_config_registry['squad'] = c
 
 c = qa_config_registry['squad']
-c['embedding_path'] = '/data/lisatmp4/bahdanau/data/squad/glove_w_specials.npy'
+c['data_path'] = '/data/lisatmp4/bahdanau/data/squad/squad_glove'
+c['emb_dim'] = 300
+c['embedding_path'] = '/data/lisatmp4/bahdanau/data/squad/squad_glove/glove_w_specials.npy'
 c['num_input_words'] = 0
 qa_config_registry['squad_glove'] = c
 
+def from1to2(c):
+    c['batch_size'] = 256
+    c['batch_size_valid'] = 256
+    c['dim'] = 200
+
+c = qa_config_registry['squad']
+from1to2(c)
+qa_config_registry['squad2'] = c
+
 c = qa_config_registry['squad_glove']
-c['batch_size'] = 256
-c['batch_size_valid'] = 256
-c['dim'] = 200
+from1to2(c)
 qa_config_registry['squad_glove2'] = c
