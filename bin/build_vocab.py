fd4d0257138b4c8f0d6a8301adaaa0fad7f28cf6
rizar <dimabv@tut.by>
2017-04-14 23:24:35 -0400

    support hdf5 input of SQuAD-like format

--- bin/build_vocab.py
+++ bin/build_vocab.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
 
+import h5py
 import argparse
 
 from dictlearn.vocab import Vocabulary
 
 def main():
-  parser = argparse.ArgumentParser("Builds a dictionary")
-  parser.add_argument("--top_k", type=int, help="Top most frequent words to leave")
-  parser.add_argument("text", help="The text to use")
-  parser.add_argument("vocab", help="Destination")
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser("Builds a dictionary")
+    parser.add_argument("--top_k", type=int, help="Top most frequent words to leave")
+    parser.add_argument("text", help="The text to use")
+    parser.add_argument("vocab", help="Destination")
+    args = parser.parse_args()
 
-  vocab = Vocabulary.build(args.text, args.top_k)
-  vocab.save(args.vocab)
+    if args.text.endswith('.h5'):
+        with h5py.File(args.text) as h5_file:
+            text = h5_file['text'][:]
+    else:
+        text = args.text
+    vocab = Vocabulary.build(text, args.top_k)
+    vocab.save(args.vocab)
 
 if __name__ == "__main__":
     main()
