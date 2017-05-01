dec1098d8746fbc266fd4659f89c117b32fdba85
rizar <dimabv@tut.by>
2017-05-01 15:26:15 -0400

    change

--- bin/cloud_train_extractive_qa.sh
+++ bin/cloud_train_extractive_qa.sh
@@ -6,4 +6,4 @@ export THEANO_FLAGS="device=gpu,compiledir=/workspace/.theano"
 export PYTHONPATH=/workspace/blocks:/workspace/fuel:/workspace/dict_based_learning
 export NLTK_DATA=/workspace/nltk_data
 
-/workspace/dict_based_learning/bin/train_extractive_qa.py $@
+/workspace/dict_based_learning/bin/train_extractive_qa.py --data_path /data/cf9ffb48-61bd-40dc-a011-b2e7e5acfd72/squad/squad_from_scratch $@
