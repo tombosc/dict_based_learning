7ce1c3233d37b9869586eb544c7da528750db8bd
rizar <dimabv@tut.by>
2017-05-02 16:55:05 -0400

    change

--- bin/cloud_train_extractive_qa.sh
+++ bin/cloud_train_extractive_qa.sh
@@ -3,7 +3,7 @@ set -x
 
 export PYTHONUNBUFFERED=1
 export THEANO_FLAGS="device=gpu,compiledir=/workspace/.theano"
-export PYTHONPATH=/workspace/blocks:/workspace/fuel:/workspace/dict_based_learning
+export PYTHONPATH=/workspace/Theano:/workspace/blocks:/workspace/fuel:/workspace/dict_based_learning
 export NLTK_DATA=/workspace/nltk_data
 
 /workspace/dict_based_learning/bin/train_extractive_qa.py $@
