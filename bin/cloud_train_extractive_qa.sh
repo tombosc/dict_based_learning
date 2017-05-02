230522b3a416ee676a42f560056ea4074d1d06db
rizar <dimabv@tut.by>
2017-05-02 18:15:51 -0400

    change the cloud script

--- bin/cloud_train_extractive_qa.sh
+++ bin/cloud_train_extractive_qa.sh
@@ -5,5 +5,6 @@ export PYTHONUNBUFFERED=1
 export THEANO_FLAGS="device=cuda,compiledir=/workspace/.theano"
 export PYTHONPATH=/workspace/Theano:/workspace/blocks:/workspace/fuel:/workspace/dict_based_learning
 export NLTK_DATA=/workspace/nltk_data
+export PATH=$PATH:/workspace/dict_based_learning/bin
 
 /workspace/dict_based_learning/bin/train_extractive_qa.py $@
