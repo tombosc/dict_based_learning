a623fb12b780587596826c5235b74bd98886ee4d
rizar <dimabv@tut.by>
2017-05-05 11:54:06 -0400

    change the cloud script

--- bin/cloud_train_extractive_qa.sh
+++ bin/cloud_train_extractive_qa.sh
@@ -8,4 +8,6 @@ export FUEL_DATA_PATH=/data/cf9ffb48-61bd-40dc-a011-b2e7e5acfd72
 export NLTK_DATA=/workspace/nltk_data
 export PATH=$PATH:/workspace/dict_based_learning/bin
 
+JOBID=${MARATHON_APP_ID:1}
+
 /workspace/dict_based_learning/bin/train_extractive_qa.py $@
