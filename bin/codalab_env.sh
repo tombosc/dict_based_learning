c3e9a7ca224b2c913831357caa75cc0be09d1405
rizar <dimabv@tut.by>
2017-05-16 15:56:36 -0400

    environment

---
+++ bin/codalab_env.sh
@@ -0,0 +1,7 @@
+export NLTK_DATA=nltk_data 
+export PYTHONIOENCODING=utf-8 
+export PYTHONPATH=dict_based_learning 
+export FUEL_DATA_PATH=data 
+export FUEL_FLOATX=float32 
+export THEANO_FLAGS=floatX=float32
+export CLASSPATH=stanford-corenlp-full-2016-10-31/*
