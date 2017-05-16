0a25dba319bab498a208375cfb0e6d15605a1ca5
rizar <dimabv@tut.by>
2017-05-16 17:39:21 -0400

    fix paths

--- bin/codalab_run.sh
+++ bin/codalab_run.sh
@@ -9,7 +9,7 @@ export THEANO_FLAGS=floatX=float32
 export CLASSPATH=/workspace/stanford-corenlp-full-2016-10-31/*
 
 mkdir -p data/squad
-ln -s squad_from_scratch data/squad/
-ln -s squad_glove data/squad
+ln -s codalab_squad/squad_from_scratch data/squad/
+ln -s codalab_squad/squad_glove data/squad
 
 /workspace/dict_based_learning/bin/eval_extractive_qa.py --part dev $@ model/training_state_best.tar
