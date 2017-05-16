6c8354fdf2c1d437253459af259d5ed40c2340e7
rizar <dimabv@tut.by>
2017-05-16 17:11:57 -0400

    codalab

--- bin/codalab_env.sh
+++ bin/codalab_env.sh
@@ -1,7 +1,13 @@
-export NLTK_DATA=nltk_data 
+export NLTK_DATA=/workspace/nltk_data 
 export PYTHONIOENCODING=utf-8 
-export PYTHONPATH=dict_based_learning 
+export PYTHONPATH=/workspace/dict_based_learning 
 export FUEL_DATA_PATH=data 
 export FUEL_FLOATX=float32 
 export THEANO_FLAGS=floatX=float32
-export CLASSPATH=stanford-corenlp-full-2016-10-31/*
+export CLASSPATH=/workspace/stanford-corenlp-full-2016-10-31/*
+
+mkdir -p data/squad
+ln -s squad_from_scratch data/squad/
+ln -s squad_glove data/squad
+
+/workspace/dict_based_learning/bin/eval_extractive_qa.py --part dev $1 model/training_state_best.tar
