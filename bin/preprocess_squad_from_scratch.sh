4b16f87861440b2a33602b7caa9aba1a693f6902
rizar <dimabv@tut.by>
2017-06-08 20:03:59 +0000

    update preprpocessing scripts

--- bin/preprocess_squad_from_scratch.sh
+++ bin/preprocess_squad_from_scratch.sh
@@ -6,10 +6,8 @@ set -x
 [ -e train.json ] || exit 1
 [ -e dev.json ] || exit 1
 
-# for CoreNLP
-export CLASSPATH=$HOME/Dist/stanford-corenlp-full-2016-10-31/* 
-
-DBL=$HOME/Dist/dict_based_learning
+[ -z "$CLASSPATH" ] && export CLASSPATH="$HOME/Dist/stanford-corenlp-full-2016-10-31/*"
+[ -z $DBL ] && DBL=$HOME/Dist/dict_based_learning
 
 $DBL/bin/pack_to_hdf5.py --type=squad train.json train.h5
 $DBL/bin/pack_to_hdf5.py --type=squad --relaxed-span dev.json dev.h5
